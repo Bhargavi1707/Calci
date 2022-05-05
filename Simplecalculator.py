ef3e10e100b7899177573833f0cf3120b70d7f16
Bhargavi <bhargavitalapalle44@gmail.com>
2022-05-05 16:23:56 +0530

    Substraction function added

--- Simplecalculator.py
+++ Simplecalculator.py
@@ -4,6 +4,13 @@
 def add(num1, num2):
     result=num1+num2
     return result
-  
+
+def sub(num1, num2):
+    result=num1-num2
+    return result
+
+
 result = add(1,2)
+print(result)
+result = sub(5,3)
 print(result)
